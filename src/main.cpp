03d94f8efac8b18cfa6e6b34f5de2854025ac32f
matsem <matej.semancik@gmail.com>
2019-10-12 02:54:26 +0200

    Add support for additional LED strip [hovnokod]

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,25 @@
 // FastLED stuff
 
 #define LEDS_DATA_PIN 27
+#define LEDS_DATA_PIN_2 26
 #define NUM_LEDS 150  // 30 LEDS x 5m on strip
 #define FPS 60
 CRGB leds[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+uint8_t fading = 0;
 
 // ArtNet stuff
 
+#define UNIVERSE_START 2
 const char* wifi_ssid = WIFI_SSID;
 const char* wifi_pass = WIFI_PASSWORD;
 ArtnetWifi artnet;
-const uint16_t num_channels = NUM_LEDS * 3;  // 1 LED = 3 channels
 
 void led_runner(void* pvparams) {
   printf("FastLED runner started.\n");
 
   FastLED.addLeds<WS2812B, LEDS_DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(255);
+  FastLED.addLeds<WS2812B, LEDS_DATA_PIN_2, GRB>(leds2, NUM_LEDS);
 
   while (1) {
     FastLED.setBrightness(200);
@@ -38,18 +41,33 @@ void dmx_callback(uint16_t universe,
                   uint16_t length,
                   uint8_t sequence,
                   uint8_t* data) {
-  
-  printf("universe: %d, len: %d\n", universe, length);
+  // printf("universe: %d, len: %d\n", universe, length);
   if (universe == 0) {
     // FFT data incoming
     return;
   }
 
-  if (universe == 1 && length <= NUM_LEDS * 3) {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    for (uint16_t i = 0; i < length / 3; i++) {
-      leds[i] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+  if (universe == 1) {
+    if (length > 0) {
+      fading = data[0];
     }
+  }
+
+  if (length <= NUM_LEDS * 3) {
+    if (universe == UNIVERSE_START) {
+      for (uint16_t i = 0; i < length / 3; i++) {
+        leds[i] += CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+      }
+    }
+
+    if (universe == UNIVERSE_START + 1) {
+      for (uint16_t i = 0; i < length / 3; i++) {
+        leds2[i] += CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+      }
+    }
+
+    fadeToBlackBy(leds, NUM_LEDS, fading);
+    fadeToBlackBy(leds2, NUM_LEDS, fading);
   }
 }
 
@@ -70,7 +88,7 @@ void setup() {
     xTaskCreatePinnedToCore(led_runner, "FastLED runner", 4096, NULL, 2, NULL,
                             CONFIG_ARDUINO_RUNNING_CORE);
 
-    xTaskCreatePinnedToCore(artnet_runner, "ArtNet runner", 4096, NULL, 2, NULL,
+    xTaskCreatePinnedToCore(artnet_runner, "ArtNet runner", 4096, NULL, 1, NULL,
                             CONFIG_ARDUINO_RUNNING_CORE);
   }
 }
