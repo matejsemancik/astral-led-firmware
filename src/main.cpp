482f5666229f4b9f1121214ea5c8a239bc8493fc
matsem <matej.semancik@gmail.com>
2020-03-14 23:00:04 +0100

    use array of FastLED controllers, one controller per DMX universe

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,9 @@
 
 // FastLED stuff
 
-#define LEDS_DATA_PIN 12
-#define NUM_LEDS 150  // 30 LEDS x 5m
-CRGB leds[NUM_LEDS];
-uint8_t fading = 0;
+#define NUM_BRANCHES 3
+#define LEDS_PER_BRANCH 150
+CRGB leds[NUM_BRANCHES][LEDS_PER_BRANCH];
 
 // ArtNet stuff
 
@@ -21,32 +20,31 @@ const char* wifi_ssid = WIFI_SSID;
 const char* wifi_pass = WIFI_PASSWORD;
 ArtnetWifi artnet;
 
-int previousDataLength = 0;
-const int startUniverse = 0;
+int prev_data_len = 0;
+const int start_universe = 0;
 
 void dmx_callback(uint16_t universe,
                   uint16_t length,
                   uint8_t sequence,
                   uint8_t* data) {
-                    
-  for (int i = 0; i < length / 3; i++)
-  {
-    int led = i + (universe - startUniverse) * (previousDataLength / 3);
-    if (led < NUM_LEDS)
-    {
-      leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+  for (int i = 0; i < length / 3; i++) {
+    int led = i + (universe - start_universe) * (prev_data_len / 3);
+    if (led < LEDS_PER_BRANCH) {
+      leds[universe][led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
     }
   }
-  previousDataLength = length;     
+  prev_data_len = length;
   FastLED.show();
 }
 
-void artnet_runner(void* pvparams) {
-  printf("ArtNet runner started.\n");
+void artnet_task(void* pvparams) {
+  printf("ArtNet task started.\n");
   artnet.begin();
   artnet.setArtDmxCallback(dmx_callback);
 
-  FastLED.addLeds<WS2812B, LEDS_DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, 12, GRB>(leds[0], LEDS_PER_BRANCH);
+  FastLED.addLeds<WS2812B, 14, GRB>(leds[1], LEDS_PER_BRANCH);
+  FastLED.addLeds<WS2812B, 27, GRB>(leds[2], LEDS_PER_BRANCH);
 
   while (1) {
     artnet.read();
@@ -57,7 +55,7 @@ void setup() {
   bool wifi_connected = connect_wifi(wifi_ssid, wifi_pass);
 
   if (wifi_connected) {
-    xTaskCreatePinnedToCore(artnet_runner, "ArtNet runner", 4096, NULL, 1, NULL,
+    xTaskCreatePinnedToCore(artnet_task, "ArtNet task", 4096, NULL, 1, NULL,
                             CONFIG_ARDUINO_RUNNING_CORE);
   }
 }
